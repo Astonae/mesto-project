6726535d5b7f1237b56f899004af87b5d318fc88
Shashkin Anton <shashkinae@gmail.com>
2022-12-29 22:01:38 +0300

    fix: two ref

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ const popupProfileCreateButton = popupProfile.querySelector('.form__button');
 const popupPicture = document.querySelector('#popup-picture');
 const popupPictureButton = popupPicture.querySelector('.popup__closed-button');
 const popupPictureCreateButton = popupPicture.querySelector('.form__button');
-const popupCloses = document.querySelectorAll('.popup__closed-button');
+const closeButtons = document.querySelectorAll('.popup__closed-button');
 const popupImg = document.querySelector('#popup-img');
 const popupImgButton = popupImg.querySelector('.popup__closed-button');
 
@@ -43,7 +43,7 @@ buttonPicture.addEventListener('click', function () {
   openPopup(popupPicture);
 });
 
-popupCloses.forEach(function (button) {
+closeButtons.forEach(function (button) {
   const popup = button.closest('.popup');
   button.addEventListener('click', function () {
     closePopup(popup);
@@ -64,15 +64,13 @@ function handleFormSubmitProfile(evt) {
 	// Вставьте новые значения с помощью textContent
 	profileInfo.firstElementChild.textContent = nameProfile.value; // Выбираем первый элемент контейнера .profile__info, меняем текст
 	profileInfo.lastElementChild.textContent = aboutProfile.value; // Выбираем последний элемент контейнера .profile__info, меняем текст
-
-  popupProfileCreateButton.addEventListener('click', function () { // Закрываем попап при клике на сохранить
-    closePopup(popupProfile);
-  });
+  closePopup(popupProfile);
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
   formCardsProfile.addEventListener('submit', handleFormSubmitProfile);
+
 
 /* ------------------------------------------------------- */
 
@@ -84,7 +82,6 @@ function createElement(link, name) {
   const cardImage = cardClone.querySelector('.card__image');
   cardImage.src = link;
   cardImage.alt = name;
-  cardClone.querySelector('.card__place');
   cardClone.querySelector('.card__name').textContent = name;
   cardClone.querySelector('.card__like').addEventListener('click', function (evt) {
     evt.target.classList.toggle('card__like_active');
@@ -108,19 +105,18 @@ buttonPicture.addEventListener('click', () => {
   openPopup(popupPicture);
 });
 
-
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function submitCardsForm(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   blockCards.prepend(createElement(linkInput.value, nameInput.value));
-  popupPictureCreateButton.addEventListener('click', function () { // Закрываем попап при клике на сохранить
-    closePopup(popupPicture);
-  });
+  closePopup(popupPicture);
   linkInput.value = '';
   nameInput.value = '';
 }
 
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
 formCardsAdd.addEventListener('submit', submitCardsForm);
 
 function openImagePopup(link, name) {
